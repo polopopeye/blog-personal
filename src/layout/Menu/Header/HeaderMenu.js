0d45fc500a38bdc901a988fe49ef318d8331d641
kenneth suarez <hardmonkeypc@gmail.com>
2022-05-24 12:10:09 +0200

    Update HeaderMenu.js

--- src/layout/Menu/Header/HeaderMenu.js
+++ src/layout/Menu/Header/HeaderMenu.js
@@ -2,24 +2,27 @@ import React from 'react';
 import MenuButton from './MenuButton';
 
 import IconsRowSocial from '../modules/IconRowSocial';
+import { useTranslation } from 'react-i18next';
 
 const HeaderMenu = () => {
+  const { t } = useTranslation();
+
   return (
     <div className="grid grid-flow-col grid-cols-1 grid-rows-4 gap-4 font-mono text-lg text-center ">
       <MenuButton
-        nombre="Proyectos"
+        nombre={t('articles.title')}
         link="/"
         classN="w-full p-4"
         classNActive="w-full p-2 text-gray-100 bg-gray-700"
       />
       <MenuButton
-        nombre="Tecnologías"
+        nombre={t('tech.title')}
         link="/tech/"
         classN="w-full p-4  "
         classNActive="w-full p-2 text-gray-100 bg-gray-700"
       />
       <MenuButton
-        nombre="Curriculum"
+        nombre={t('curriculum.title')}
         link="/cv/"
         classN="w-full p-4  "
         classNActive="w-full p-2 text-gray-100 bg-gray-700"
